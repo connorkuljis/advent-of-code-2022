5340cb64537f307aa28f484dbc9a9801b0ec0de3
connorkuljis <conkuljis@gmail.com>
2022-12-02 15:59:31 +0800

    init - from day 2 part 1

---
+++ days/01-elves-kcal/index.ts
@@ -0,0 +1,35 @@
+// TypeScript:
+
+import { readFileSync } from 'fs';
+
+const file = readFileSync('./items.txt', 'utf-8');
+const lines = file.split('\n');
+
+let idx = 0;
+let sum = 0;
+
+let elves: Array<{ id: number, sum: number }> = [];
+
+for (let i = 0; i < lines.length; i++) {
+    if (lines[i].length == 0) {
+        elves.push({ id: idx, sum: sum });
+        sum = 0;
+        idx++
+    }
+    else {
+        sum += parseInt(lines[i]);
+    }
+}
+
+const sortedArray: { id: number, sum: number }[] =
+    elves.sort((a, b) => a.sum - b.sum);
+
+let kcal_total = 0;
+
+kcal_total += sortedArray.pop()!.sum;
+kcal_total += sortedArray.pop()!.sum;
+kcal_total += sortedArray.pop()!.sum;
+
+console.log(kcal_total)
+
+
