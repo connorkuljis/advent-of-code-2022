fb0f232f5d6e59fd7474cab10eb10d330cbf6029
Connor <conkuljis@gmail.com>
2022-12-04 17:10:47 +0800

    simplify overlap expression

--- days/04-Camp-Cleanup/index.ts
+++ days/04-Camp-Cleanup/index.ts
@@ -1,8 +1,4 @@
 import {readFileSync} from "fs";
-
-const file = readFileSync("input.txt", "utf-8");
-const lines = file.split("\n");
-
 function stringPairToArray(s: string) {
     let split = s.split('-')
     return split.map(e => parseInt(e));
@@ -12,25 +8,18 @@ function isPairHasCompleteOverlap(pair: number[][]) {
     let a = pair[0];
     let b = pair[1];
 
-    let overlap = false;
-    if (a[0] <= b[0] && a[1] >= b[1])
-    {
-        overlap = true;
-    }
-    else if (b[0] <= a[0] && b[1] >= a[1])
-    {
-        overlap = true;
-        console.log(pair)
-    }
-    return overlap
+    return ((a[0] <= b[0] && a[1] >= b[1]) || (b[0] <= a[0] && b[1] >= a[1]))
 }
 
-const pairs = lines.map(e => e.split(','));
-let res = pairs.map(nested => nested.map(e => stringPairToArray(e)));
+const file = readFileSync("input.txt", "utf-8");
+const lines = file.split("\n");
+
+const split: string[][] = lines.map(e => e.split(',')); //
+let pairs: number[][][] = split.map(nested => nested.map(e => stringPairToArray(e)));
 
 let ans = 0;
-for (let i = 0; i < res.length; i++) {
-    if (isPairHasCompleteOverlap(res[i]))
+for (let i = 0; i < pairs.length; i++) {
+    if (isPairHasCompleteOverlap(pairs[i]))
     {
         ans = ans + 1;
     }
